904e7dd9933f0b99c9e2ff1193678f441dc78753
zcompgeek <zach.l.costa@gmail.com>
2014-07-31 10:15:14 -0400

    Initial Commit

---
+++ RoboRaceTests/RoboRaceTests.swift
@@ -0,0 +1,36 @@
+//
+//  RoboRaceTests.swift
+//  RoboRaceTests
+//
+//  Created by Zach Costa on 7/31/14.
+//  Copyright (c) 2014 Zach Costa. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RoboRaceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
